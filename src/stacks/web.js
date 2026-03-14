8957c1a552b9f35ba0884e881e46b94103cdce05
Cuatro <luigi@cuatro.dev>
2026-03-14 13:04:21 -0500

    refactor: Split stack.js into per-stack-type data files

---
+++ src/stacks/web.js
@@ -0,0 +1,197 @@
+export const JS_FRAMEWORKS = [
+  { name: 'Angular', icon: 'no-icon.png', website: 'https://angular.dev/' },
+  { name: 'Backbone', icon: 'no-icon.png', website: 'http://backbonejs.org/' },
+  { name: 'Ember.js', icon: 'no-icon.png', website: 'http://emberjs.com/' },
+  { name: 'jQuery', icon: 'no-icon.png', website: 'https://jquery.com/' },
+  { name: 'React', icon: 'no-icon.png', website: 'https://react.dev/' },
+  { name: 'Vue.js', icon: 'no-icon.png', website: 'http://vuejs.org/' },
+  { name: 'Svelte', icon: 'no-icon.png', website: 'https://svelte.dev/' },
+  { name: 'Mithril', icon: 'no-icon.png', website: 'https://mithril.js.org/' },
+  { name: 'Aurelia', icon: 'no-icon.png', website: 'http://aurelia.io/' },
+  { name: 'CanJS', icon: 'no-icon.png', website: 'https://canjs.com/' },
+  {
+    name: 'Polymer',
+    icon: 'no-icon.png',
+    website: 'https://www.polymer-project.org/',
+  },
+  { name: 'Preact', icon: 'no-icon.png', website: 'https://preactjs.com/' },
+  { name: 'Lit', icon: 'no-icon.png', website: 'https://lit.dev/' },
+  { name: 'SolidJS', icon: 'no-icon.png', website: 'https://www.solidjs.com/' },
+  { name: 'Qwik', icon: 'no-icon.png', website: 'https://qwik.dev/' },
+  { name: 'Alpine.js', icon: 'no-icon.png', website: 'https://alpinejs.dev/' },
+  {
+    name: 'Stimulus',
+    icon: 'no-icon.png',
+    website: 'https://stimulus.hotwired.dev/',
+  },
+  { name: 'htmx', icon: 'no-icon.png', website: 'https://htmx.org/' },
+  { name: 'Astro', icon: 'no-icon.png', website: 'https://astro.build/' },
+  { name: 'Gatsby', icon: 'no-icon.png', website: 'https://www.gatsbyjs.com/' },
+  { name: 'Next.js', icon: 'no-icon.png', website: 'https://nextjs.org/' },
+  { name: 'Nuxt.js', icon: 'no-icon.png', website: 'https://nuxt.com/' },
+  { name: 'Remix', icon: 'no-icon.png', website: 'https://remix.run/' },
+  { name: 'Stencil', icon: 'no-icon.png', website: 'https://stenciljs.com/' },
+];
+
+export const JS_PREPROCESSORS = [
+  {
+    name: 'Vanilla JS',
+    icon: 'no-icon.png',
+    website: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+  },
+  {
+    name: 'TypeScript',
+    icon: 'no-icon.png',
+    website: 'https://www.typescriptlang.org/',
+  },
+  {
+    name: 'CoffeeScript',
+    icon: 'no-icon.png',
+    website: 'http://coffeescript.org/',
+  },
+];
+
+export const CSS_FRAMEWORKS = [
+  {
+    name: 'Bootstrap',
+    icon: 'no-icon.png',
+    website: 'http://getbootstrap.com/',
+  },
+  {
+    name: 'Semantic UI',
+    icon: 'no-icon.png',
+    website: 'http://semantic-ui.com/',
+  },
+  {
+    name: 'Foundation',
+    icon: 'no-icon.png',
+    website: 'http://foundation.zurb.com/',
+  },
+  {
+    name: 'Materialize',
+    icon: 'no-icon.png',
+    website: 'http://materializecss.com/',
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: 'no-icon.png',
+    website: 'https://tailwindcss.com/',
+  },
+  { name: 'Bulma', icon: 'no-icon.png//bulma.io/' },
+  { name: 'UIkit', icon: 'no-icon.png//getuikit.com/' },
+  {
+    name: 'Chakra UI',
+    icon: 'no-icon.png',
+    website: 'https://chakra-ui.com/',
+  },
+  {
+    name: 'Material UI',
+    icon: 'no-icon.png',
+    website: 'https://mui.com/',
+  },
+  {
+    name: 'Ant Design',
+    icon: 'no-icon.png',
+    website: 'https://ant.design/',
+  },
+  {
+    name: 'shadcn/ui',
+    icon: 'no-icon.png',
+    website: 'https://ui.shadcn.com/',
+  },
+  { name: 'Mantine', icon: 'no-icon.png//mantine.dev/' },
+  { name: 'daisyUI', icon: 'no-icon.png//daisyui.com/' },
+  {
+    name: 'Radix UI',
+    icon: 'no-icon.png',
+    website: 'https://www.radix-ui.com/',
+  },
+  {
+    name: 'Headless UI',
+    icon: 'no-icon.png',
+    website: 'https://headlessui.com/',
+  },
+  {
+    name: 'Flowbite',
+    icon: 'no-icon.png',
+    website: 'https://flowbite.com/',
+  },
+  {
+    name: 'Vuetify',
+    icon: 'no-icon.png',
+    website: 'https://vuetifyjs.com/',
+  },
+  {
+    name: 'PrimeVue',
+    icon: 'no-icon.png',
+    website: 'https://primevue.org/',
+  },
+  { name: 'Pico CSS', icon: 'no-icon.png//picocss.com/' },
+  {
+    name: 'Milligram',
+    icon: 'no-icon.png',
+    website: 'https://milligram.io/',
+  },
+  {
+    name: 'Open Props',
+    icon: 'no-icon.png',
+    website: 'https://open-props.style/',
+  },
+  { name: 'Tachyons', icon: 'no-icon.png//tachyons.io/' },
+  {
+    name: 'Spectre.css',
+    icon: 'no-icon.png',
+    website: 'https://picturepan2.github.io/spectre/',
+  },
+  {
+    name: 'Water.css',
+    icon: 'no-icon.png',
+    website: 'https://watercss.kognise.dev/',
+  },
+  { name: 'Pure.css', icon: 'no-icon.png//purecss.io/' },
+  {
+    name: 'Ionic',
+    icon: 'no-icon.png',
+    website: 'https://ionicframework.com/',
+  },
+];
+
+export const CSS_PREPROCESSORS = [
+  {
+    name: 'Vanilla CSS',
+    icon: 'no-icon.png',
+    website: 'http://www.w3.org/TR/CSS/',
+  },
+  { name: 'Less', icon: 'no-icon.png', website: 'http://lesscss.org/' },
+  { name: 'Sass', icon: 'no-icon.png', website: 'http://sass-lang.com/' },
+  {
+    name: 'Stylus',
+    icon: 'no-icon.png',
+    website: 'https://learnboost.github.io/stylus/',
+  },
+  {
+    name: 'cssnext',
+    icon: 'no-icon.png',
+    website: 'https://cssnext.github.io/',
+  },
+  {
+    name: 'PostCSS',
+    icon: 'no-icon.png',
+    website: 'https://postcss.org/',
+  },
+  {
+    name: 'CSS Modules',
+    icon: 'no-icon.png',
+    website: 'https://github.com/css-modules/css-modules',
+  },
+  {
+    name: 'vanilla-extract',
+    icon: 'no-icon.png',
+    website: 'https://vanilla-extract.style/',
+  },
+  {
+    name: 'Linaria',
+    icon: 'no-icon.png',
+    website: 'https://linaria.dev/',
+  },
+];
